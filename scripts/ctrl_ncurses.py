90910cdf1dee26ca14bd915c27286f42724f888e
James Lynch <jctlynch@gmail.com>
2021-01-15 20:05:28 -0500

    Finished two functions, one for drawing a horiz servo ctl and one for drawing servo icons

--- scripts/ctrl_ncurses.py
+++ scripts/ctrl_ncurses.py
@@ -9,10 +9,23 @@ from time import sleep
 parser = argparse.ArgumentParser()
 parser.add_argument("--test", nargs="?", const=True, default=False)
 
+# Unicode box
+# https://stackoverflow.com/questions/46063974/printing-extended-ascii-characters-in-python
+DOUBLE_LEFT_TOP = u'\u2554'
+DOUBLE_VERTI_PIPE = u'\u2551'
+DOUBLE_LEFT_BOTTOM = u'\u255a'
+DOUBLE_RIGHT_TOP = u'\u2557'
+DOUBLE_RIGHT_BOTTOM =u'\u255d'
+DOUBLE_HORIZ_PIPE = u'\u2550'
+DOUBLE_VERTI_PIPE = u'\u2551'
+CENTER = u'\u2573'
+
 LOG_FILE = open("stdout.txt", "w")
+SERVO_ASCII_3X3  = DOUBLE_LEFT_TOP + DOUBLE_HORIZ_PIPE + DOUBLE_RIGHT_TOP + "\n" + DOUBLE_VERTI_PIPE + CENTER + \
+                   DOUBLE_VERTI_PIPE + "\n" + DOUBLE_LEFT_BOTTOM + DOUBLE_HORIZ_PIPE + DOUBLE_RIGHT_BOTTOM
+SERVO_ASCII_1X1 = "X"
 
-
-def draw_vertical_servo(servo, window, title="Servo"):
+def draw_vertical_servo_ctl(servo, window, title="Servo"):
     """
     Draw the servos on the given window, filling in the initial value.
     
@@ -34,25 +47,80 @@ def draw_vertical_servo(servo, window, title="Servo"):
 
     for y in range(2, avail_height): #start at two to account for the title
         if y == 2:
-            window.addstr(y, midpoint-1, "[{:.2f}]".format(curr_val))
+            window.addstr(y, midpoint-2, "[{:.2f}]".format(curr_val))
         elif y == avail_height-1:
             window.addstr(y, midpoint-1, "[0]")
 
         else:
             # Determine whether or not the servo level should fill this bar as much 
             if y / avail_height >= curr_val:
-                window.addstr(y, midpoint-1, "|=|")
+                window.addstr(y, midpoint-1, "[+]")
             else:
                 window.addstr(y, midpoint-1, "| |")
     # window.refresh()
             
-    
+def draw_horizontal_servo_ctl(servo, window, title="Horizontal Servo"):
+    """
+    Draws a progress bar similar to draw_vertical_servo, but at a 90 degree offset.
+    This will usually be used for the twist servo.
+    :param Servo servo: The Servo object to grab a value from
+    :param curses.Window window: The window object we will build the horizontal window in
+    :param title: The title for the servo controller
+    :return:
+    """
+    # Give the window a title
+    add_title(window, title)
+    # window.addstr(window.getmaxyx().__str__())
+
+    # Create a horizontal bar based on the current value of the servo
+    # Use the entire width, minus a header/footer and padding on each side
+    height, avail_width = window.getmaxyx()
+    avail_width -= 1
+    midpoint = height // 2
+
+    # Get the current value of the servo as a threshold
+    curr_val = servo.value
+    avail_width -= 4 # Account for headers on left and right
+    for x in range(2, avail_width):  # end 4 early to account for headers
+        if x == 2:
+            window.addstr(midpoint + 1, x,  "[1]".format(curr_val))
+        elif x in {2, 3, 4}:
+            pass # Would overwrite l72
+        elif x == avail_width - 1:
+            window.addstr(midpoint + 1, x, "[0]")
+            break
+
+        else:
+            # Determine whether or not the servo level should fill this bar as much
+            if x / avail_width <= curr_val:
+                window.addstr(midpoint + 1, x, "|")
+            else:
+                window.addstr(midpoint + 1, x, " ")
     
 
 
-    
-    
-    
+def draw_servo(y, x, window, servo, icon=SERVO_ASCII_3X3):
+    """
+    Draws a servo's location (represented by an ascii icon) on the given window, centered around x, y
+    :param y:
+    :param x:
+    :param window:
+    :param servo:
+    :return:
+    """
+    if icon == SERVO_ASCII_3X3:
+        xoffset = yoffset = 3
+    elif icon == SERVO_ASCII_1X1:
+        xoffset = yoffset = 0
+    else:
+        # Best guess
+        xoffset = yoffset = len(icon) // max(icon.count("\n"), 1)
+
+    # Curses doesn't work super well with strings that contain newlines
+    # window.addstr(max(y-yoffset, 0), max(x-xoffset, 0), icon)
+    for line in icon.splitlines(keepends=False):
+        window.addstr(y - yoffset, x - xoffset, line)
+        yoffset -= 1
 
 
 def add_title(window, title):
@@ -136,17 +204,20 @@ def main(stdscr):
     # servo_ctl.refresh()
     
     # Draw the current servo levels
-    draw_vertical_servo(head, h_ctl, "Head Servo")
-    draw_vertical_servo(neck,n_ctl, "Neck Servo")
-    draw_vertical_servo(arm, a_ctl, "Arm Servo")
+    draw_vertical_servo_ctl(head, h_ctl, "Head Servo")
+    draw_vertical_servo_ctl(neck, n_ctl, "Neck Servo")
+    draw_vertical_servo_ctl(arm, a_ctl, "Arm Servo")
+    draw_horizontal_servo_ctl(twist, t_ctl, "Twist Servo")
 
+    # Draw the ASCIIART servo
+    draw_servo(10,10,servo_view, head)
 
     servo_ctl.bkgd(' ', curses.color_pair(1) | curses.A_BOLD | curses.A_REVERSE)
     # Apply the background colors to all writes that have occured
     servo_ctl.attron(curses.color_pair(1) | curses.A_BOLD | curses.A_REVERSE)
-
+    servo_view.refresh()
     servo_ctl.getch()
-    
+
 
     
 
@@ -155,3 +226,4 @@ def main(stdscr):
 
 if __name__ == '__main__':
     curses.wrapper(main)
+    print(SERVO_ASCII_3X3)
